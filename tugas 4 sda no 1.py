7c42c24ac3ed1390a1b1aad1a154d80535ce7ecf
claudea17 <claudeatalangi@gmail.com>
2025-05-21 11:59:22 +0800

    Add files via upload

---
+++ tugas 4 sda no 1.py
@@ -0,0 +1,16 @@
+def isBalanced(s: str) -> bool:
+    stack = []
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in bracket_map.values():  
+            stack.append(char)
+        elif char in bracket_map:  
+            if not stack or stack[-1] != bracket_map[char]:
+                return False
+            stack.pop()
+    return not stack
+
+
+print(isBalanced("([]{})"))   # True
+print(isBalanced("[({)]"))    # False
